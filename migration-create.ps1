c91e016a07928bca28efa7b9a88d8c3bce494e70
Thales Alves <thales@thalesalv.es>
2025-01-15 23:52:12 +0000

    improved migration create script to include new migration to changelog xml

--- migration-create.ps1
+++ migration-create.ps1
@@ -2,15 +2,26 @@ param (
     [string]$migrationName
 )
 
+$xmlNamespace = "http://www.liquibase.org/xml/ns/dbchangelog/1.9"
+$xmlFilePath = ".\src\main\resources\db\changelog\changelog-master.xml"
+$xmlMigrationBaseUrl = "db/changelog/migrations"
 $templateFilePath = ".\src\main\resources\db\changelog\sample_migration.sql"
 $destinationFolder = ".\src\main\resources\db\changelog\migrations"
 $timestamp = [DateTimeOffset]::Now.ToUnixTimeSeconds()
 $newFileName = "${timestamp}_${migrationName}.sql"
 $newFilePath = Join-Path -Path $destinationFolder -ChildPath $newFileName
 
-echo "Creating migration with name ${newFileName}..."
+Write-Output "Creating migration with name ${newFileName}..."
 
 Copy-Item -Path $templateFilePath -Destination $newFilePath
 (Get-Content -Path $newFilePath) -replace 'create_sample_table', "${timestamp}_${migrationName}" | Set-Content -Path $newFilePath
 
-echo "Migration with name ${newFileName} created."
+$xml = [xml](get-content $xmlFilePath)
+
+$newMigration = $xml.CreateElement("include", $xmlNamespace)
+$newMigration.SetAttribute("file","$xmlMigrationBaseUrl/$newFileName")
+
+$xml.databaseChangeLog.AppendChild($newMigration) | out-null
+$xml.save($xmlFilePath)
+
+Write-Output "Migration with name ${newFileName} created."
